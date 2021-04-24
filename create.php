9129a80cdfd654b723a664701a339e8ede796e04
Mario Hartleb <mhartleb83@yahoo.com>
2021-04-24 08:48:23 +0200

    commit 24.04. morning

--- create.php
+++ create.php
@@ -1,41 +1,79 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <?php require_once 'components/boot.php'?>
-        <title>PHP CRUD  |  Add Product</title>
-        <style>
-            fieldset {
-                margin: auto;
-                margin-top: 100px;
-                width: 60% ;
-            }       
-        </style>
-    </head>
-    <body>
-        <fieldset>
-            <legend class='h2'>Add Product</legend>
+<html lang="en" >
+   <head>
+       <meta charset="UTF-8">
+        <meta name="viewport" content ="width=device-width, initial-scale=1.0">
+       <?php require_once 'components/boot.php'?>
+       <link rel="stylesheet" href="css/styles.css">
+       <title>CR10 Mario CRUD</title>
+       <style>
+           fieldset {
+               margin: auto;
+               margin-top: 100px;
+               width: 60% ;
+           }      
+       </style>
+   </head>
+   <body>
+   <fieldset>
+            <legend class='h2'>Add Media</legend>
             <form action="actions/a_create.php" method= "post" enctype="multipart/form-data">
                 <table class='table'>
                     <tr>
-                        <th>Name</th>
-                        <td><input class='form-control' type="text" name="name"  placeholder="Product Name" /></td>
+                        <th>Title</th>
+                        <td><input class='form-control' type="text" name="title"  placeholder="Title" /></td>
                     </tr>    
                     <tr>
-                        <th>Price</th>
-                        <td><input class='form-control' type="number" name= "price" placeholder="Price" step="any" /></td>
+                        <th>First Name Author/Artist/Director</th>
+                        <td><input class='form-control' type="text" name="authFirstName"  placeholder="First Name or -" /></td>
+                    </tr>
+                    <tr>
+                        <th>Last Name Author/Artist/Director</th>
+                        <td><input class='form-control' type="text" name="authLastName"  placeholder="Last Name or Band" /></td>
+                    </tr>
+                    <tr>
+                        <th>ISBN</th>
+                        <td><input class='form-control' type="text" name="ISBN"  placeholder="ISBN or -" /></td>
+                    </tr>
+                    <tr>
+                        <th>Description</th>
+                        <td><input class='form-control' type="text" name="description"  placeholder="Description" /></td>
+                    </tr>
+                    <tr>
+                        <th>Publish Date</th>
+                        <td><input class='form-control' type="date" name="publishDate"  placeholder="Publish Date" /></td>
+                    </tr>
+                    <tr>
+                        <th>Media Type</th>
+                        <td><input class='form-control' type="text" name="mediaType"  placeholder="Media Type" /></td>
+                    </tr>
+                    <tr>
+                        <th>Availability Status</th>
+                        <td><input class='form-control' type="text" name="status"  placeholder="Status" /></td>
+                    </tr>
+                    <tr>
+                        <th>Publisher</th>
+                        <td><input class='form-control' type="text" name="pubName"  placeholder="Publisher" /></td>
+                    </tr>
+                    <tr>
+                        <th>Publisher Address</th>
+                        <td><input class='form-control' type="text" name="pubAddress"  placeholder="Publisher Address" /></td>
+                    </tr>
+                    <tr>
+                        <th>Publisher Size</th>
+                        <td><input class='form-control' type="text" name="pubSize"  placeholder="Publisher Size" /></td>
                     </tr>
                     <tr>
                         <th>Picture</th>
-                        <td><input class='form-control' type="file" name="picture" /></td>
+                        <td><input class='form-control' type="file" name="image" /></td>
                     </tr>
                     <tr>
-                        <td><button class='btn btn-success' type="submit">Insert Product</button></td>
+                        <td><button class='btn btn-success' type="submit">Insert Media</button></td>
                         <td><a href="index.php"><button class='btn btn-warning' type="button">Home</button></a></td>
                     </tr>
+                    
                 </table>
             </form>
         </fieldset>
-    </body>
+   </body>
 </html>
